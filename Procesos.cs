b0797e2afef3b39217588743b6fe53a4d9cb2e35
Barcenas80 <91450856+Barcenas80@users.noreply.github.com>
2021-09-27 00:47:37 -0500

    Add files via upload

---
+++ Procesos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Áreas
+{
+    public class Procesos
+    {
+        public decimal numero1 { get; set; }
+        public decimal numero2 { get; set; }
+        public double radio { get; set; }
+        public decimal cuadrado()
+        {
+            return numero1 * numero1;
+        }
+        public decimal triangulo()
+        {
+            return (numero1 * numero2) / 2;
+        }
+        public double circulo()
+        {
+            return (radio * radio) * 3.1416;
+        }
+    }
+}
---
+++ Áreas.asmx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Áreas
+{
+    /// <summary>
+    /// Descripción breve de Áreas
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Áreas : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public decimal cuadrado(decimal lado)
+        {
+            Procesos p = new Procesos();
+            p.numero1 = lado;
+            return p.cuadrado();
+        }
+        [WebMethod]
+        public decimal triangulo(decimal bas, decimal alt)
+        {
+            Procesos p = new Procesos();
+            p.numero1 = bas;
+            p.numero2 = alt;
+            return p.triangulo();
+        }
+        [WebMethod]
+        public double circulo(double radio)
+        {
+            Procesos p = new Procesos();
+            p.radio = radio;
+            return p.circulo();
+        }
+    }
+}
